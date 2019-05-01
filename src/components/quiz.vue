8787f9d96739c96d7407d33587b4200ded081023
rio7z <kane@riotz.co.uk>
2019-05-01 22:34:37 +0100

    port to Vue

--- src/components/quiz.vue
+++ src/components/quiz.vue
@@ -7,11 +7,11 @@
           <div id="score"></div>
           <div id="answer"></div>
           <div id="flash"></div>
-          <input id="guess" type="text" placeholder="Your guess...">
+          <input id="guess" maxlength="1" type="text" placeholder="Your guess...">
           <br>
-          <button id="start" onclick="start()">Start</button>
-          <button id="submit" onclick="guessAssign()">Guess</button>
-          <button id="repeat" onclick="repeatSeq()">Repeat</button>
+          <button id="start" v-on:click="start">Start</button>
+          <button id="submit" v-on:click="guessAssign">Guess</button>
+          <button id="repeat" v-on:click="repeatSeq">Repeat</button>
         </div>
       </section>
     </div>
@@ -19,12 +19,116 @@
 </template>
 
 <script>
+
 export default {
-  data() {
+  data: function(){
     return {
-    };
-  },
-  methods: {},
+      morse: {
+        0:['dash','dash','dash','dash','dash'],1:['dot','dash','dash','dash','dash'],2:['dot','dot','dash','dash','dash'],3:['dot','dot','dot','dash','dash'],4:['dot','dot','dot','dot','dash'],5:['dot','dot','dot','dot','dot'],6:['dash','dot','dot','dot','dot'],7:['dash','dash','dot','dot','dot'],8:['dash','dash','dash','dot','dot'],9:['dash','dash','dash','dash','dot' ],A:['dot','dash'],B:['dash','dot','dot','dot'],C:['dash','dot','dash','dot'],D:['dash','dot','dot'],E:['dot'],F:['dot','dot','dash','dot'],G:['dash','dash','dot'],H:['dot','dot','dot','dot'],I:['dot','dot'],J:['dot','dash','dash','dash'],K:['dash','dot','dash'],L:['dot','dash','dot','dot'],M:['dash','dash'],N:['dash','dot'],O:['dash','dash','dash'],P:['dot','dash','dash','dot'],Q:['dash','dash','dot','dash'],R:['dot','dash','dot'],S:['dot','dot','dot'],T:['dash'],U:['dot','dot','dash'],V:['dot','dot','dot','dash'],W:['dot','dash','dash'],X:['dash','dot','dot','dash'],Y:['dash','dot','dash','dash'],Z:['dash','dash','dot','dot']
+    },
+    pass: [],
+    fail: [],
+    guessAssigned:[],
+    sequence:[],
+    timeUnit: 500,
+    amount: 5
+    }},
+    
+  methods: {
+    start: function (){
+    const score = document.getElementById('score')
+    const submit = document.getElementById('submit')
+    const start = document.getElementById('start')
+    const guess = document.getElementById('guess')
+    
+    score.innerHTML = `Correct ${this.pass.length} | Incorrect ${this.fail.length}`
+    start.style.display = "none"
+    submit.style.display = "none"
+    repeat.style.display = "none"
+    guess.style.display = "block"
+
+    this.sequence = [];
+
+    (this.pass.length + this.fail.length == this.amount) 
+    ? this.end()
+    : this.continue()
+    },
+
+    continue: function () {
+
+    this.randomMorse(this.morse)
+    this.showMorse(this.sequence)
+    setTimeout(() => {submit.style.display = "inline-block", repeat.style.display = "inline-block"}, this.timeUnit * 20)
+    },
+
+    end: function (){
+      alert('finished')
+    },
+
+    repeatSeq: function (){
+    event.preventDefault()
+
+    const repeat = document.getElementById('repeat')
+
+    submit.style.display = "none"
+    repeat.style.display = "none"
+
+    this.showMorse(this.sequence)
+    setTimeout(() => {
+        submit.style.display = "inline-block",
+        repeat.style.display = "inline-block"
+    }, this.timeUnit * 20)
+    },
+
+    guessAssign: function (){
+    event.preventDefault()
+    this.guessAssigned = document.getElementById('guess').value.toUpperCase()
+
+    this.compareGuess(this.guessAssigned, this.sequence)
+    document.getElementById("guess").value = ''
+    this.start()
+    },
+    randomMorse: function (morse) {
+    let keys = Object.keys(morse)
+    let letter = keys[keys.length * Math.random() << 0 ]
+    let code = this.morse[letter[letter.length * Math.random() << 0 ]]
+
+    this.sequence.push(letter, code)
+    },
+
+    compareGuess: function (guessAssigned, sequence) {
+    const answer = document.getElementById('answer');
+
+    (this.guessAssigned != this.sequence[0])
+    ? (answer.innerHTML = 'wrong',
+    setTimeout(() => {
+        answer.innerHTML = ''
+    }, this.timeUnit * 2),
+    this.fail.push(this.sequence[0]))
+    : (answer.innerHTML = 'right',
+    setTimeout(() => {
+        answer.innerHTML = ''
+    }, this.timeUnit * 2),
+    this.pass.push(this.sequence[0]))
+    },
+
+    showMorse: function (sequence){
+    const flash = document.getElementById('flash')
+
+      function addFlash(time) {
+          flash.className = ''
+          flash.className += 'flash'
+          setTimeout(() => {flash.className = ''}, this.timeUnit * time)
+      }
+      this.sequence[1].forEach((element, index) => {
+        setTimeout(() => {
+            (element == 'dash') ? addFlash(3) : addFlash(1)
+        }, this.timeUnit * 4 * (index + 1))
+        })
+        }
+
+  }
+}
 </script>
 
 <style>
